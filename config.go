2baa79b1c83acede5e2e67ea9d4c096960abd890
itsmontoya <josh@meteora.co>
2018-05-10 20:11:19 -0400

    Implement configuration

---
+++ config.go
@@ -0,0 +1,10 @@
+package httpserve
+
+import "time"
+
+// Config is the basic configuration
+type Config struct {
+	ReadTimeout    time.Duration
+	WriteTimeout   time.Duration
+	MaxHeaderBytes int
+}
