4820848138b9a3ed9923863ed37bb7a6f28e1777
Josh Montoya <josh@montoya.io>
2024-04-05 11:56:58 -0700

    feature: use less tabs

--- config.go
+++ config.go
@@ -17,8 +17,8 @@ type AutoCertConfig struct {
 	DirCache string
 
 	// For TLS hosts, use either Hosts or HostsFn depending on your use-case:
-	//		- Use Hosts when your list is static
-	//		- Host HostsFn when your list is dynamic
+	//	- Use Hosts when your list is static
+	//	- Host HostsFn when your list is dynamic
 	// Hosts is a static list of acceptable TLS hosts
 	Hosts []string
 	// HostsFn is a TLS host policy that can be dynamically updated
