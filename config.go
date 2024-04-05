c8ae1209a50f73179eae07c1d392ca4b9188ea43
Josh Montoya <josh@montoya.io>
2024-04-05 12:09:29 -0700

    HostsFn -> HostPolicy

--- config.go
+++ config.go
@@ -16,18 +16,18 @@ type Config struct {
 type AutoCertConfig struct {
 	DirCache string
 
-	// For TLS hosts, use either Hosts or HostsFn depending on your use-case:
+	// For TLS hosts, use either Hosts or HostPolicy depending on your use-case:
 	//	- Use Hosts when your list is static
-	//	- Host HostsFn when your list is dynamic
+	//	- Host HostPolicy when your list is dynamic
 	// Hosts is a static list of acceptable TLS hosts
 	Hosts []string
-	// HostsFn is a TLS host policy that can be dynamically updated
-	HostsFn autocert.HostPolicy
+	// HostPolicy is a TLS host policy that can be dynamically updated
+	HostPolicy autocert.HostPolicy
 }
 
 func (a *AutoCertConfig) hostPolicy() autocert.HostPolicy {
-	if a.HostsFn != nil {
-		return a.HostsFn
+	if a.HostPolicy != nil {
+		return a.HostPolicy
 	}
 
 	return autocert.HostWhitelist(a.Hosts...)
